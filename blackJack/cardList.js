5725b434bd8f801026b17443fb5b3dd5baa9ec5d
BoggyCo <108748265+BoggyCo@users.noreply.github.com>
2023-02-27 20:30:39 -0500

    2/27

---
+++ blackJack/cardList.js
@@ -0,0 +1,363 @@
+export default [
+  [
+    {
+      type: "Ace",
+      suit: "Spades",
+      value: 11,
+      src: "./cards/ace_of_spades.png",
+    },
+    {
+      type: "Ace",
+      suit: "Clubs",
+      value: 11,
+      src: "./cards/ace_of_clubs.png",
+    },
+    {
+      type: "Ace",
+      suit: "Hearts",
+      value: 11,
+      src: "./cards/ace_of_hearts.png",
+    },
+    {
+      type: "Ace",
+      suit: "Diamonds",
+      value: 11,
+      src: "./cards/ace_of_diamonds.png",
+    },
+    {
+      type: 2,
+      suit: "Spades",
+      value: 2,
+      src: "./cards/2_of_spades.png",
+    },
+    {
+      type: 2,
+      suit: "Clubs",
+      value: 2,
+      src: "./cards/2_of_clubs.png",
+    },
+    {
+      type: 2,
+      suit: "Hearts",
+      value: 2,
+      src: "./cards/2_of_hearts.png",
+    },
+    {
+      type: 2,
+      suit: "Diamonds",
+      value: 2,
+      src: "./cards/2_of_diamonds.png",
+    },
+    {
+      type: 3,
+      suit: "Spades",
+      value: 3,
+      src: "./cards/3_of_spades.png",
+    },
+    {
+      type: 3,
+      suit: "Clubs",
+      value: 3,
+      src: "./cards/3_of_clubs.png",
+    },
+    {
+      type: 3,
+      suit: "Hearts",
+      value: 3,
+      src: "./cards/3_of_hearts.png",
+    },
+    {
+      type: 3,
+      suit: "Diamonds",
+      value: 3,
+      src: "./cards/3_of_diamonds.png",
+    },
+    {
+      type: 4,
+      suit: "Spades",
+      value: 4,
+      src: "./cards/4_of_spades.png",
+    },
+    {
+      type: 4,
+      suit: "Clubs",
+      value: 4,
+      src: "./cards/4_of_clubs.png",
+    },
+    {
+      type: 4,
+      suit: "Hearts",
+      value: 4,
+      src: "./cards/4_of_hearts.png",
+    },
+    {
+      type: 4,
+      suit: "Diamonds",
+      value: 4,
+      src: "./cards/4_of_diamonds.png",
+    },
+    {
+      type: 5,
+      suit: "Spades",
+      value: 5,
+      src: "./cards/5_of_spades.png",
+    },
+    {
+      type: 5,
+      suit: "Clubs",
+      value: 5,
+      src: "./cards/5_of_clubs.png",
+    },
+    {
+      type: 5,
+      suit: "Hearts",
+      value: 5,
+      src: "./cards/5_of_hearts.png",
+    },
+    {
+      type: 5,
+      suit: "Diamonds",
+      value: 5,
+      src: "./cards/5_of_diamonds.png",
+    },
+    {
+      type: 6,
+      suit: "Spades",
+      value: 6,
+      src: "./cards/6_of_spades.png",
+    },
+    {
+      type: 6,
+      suit: "Clubs",
+      value: 6,
+      src: "./cards/6_of_clubs.png",
+    },
+    {
+      type: 6,
+      suit: "Hearts",
+      value: 6,
+      src: "./cards/6_of_hearts.png",
+    },
+    {
+      type: 6,
+      suit: "Diamonds",
+      value: 6,
+      src: "./cards/6_of_diamonds.png",
+    },
+    {
+      type: 7,
+      suit: "Spades",
+      value: 7,
+      src: "./cards/7_of_spades.png",
+    },
+    {
+      type: 7,
+      suit: "Clubs",
+      value: 7,
+      src: "./cards/7_of_clubs.png",
+    },
+    {
+      type: 7,
+      suit: "Hearts",
+      value: 7,
+      src: "./cards/7_of_hearts.png",
+    },
+    {
+      type: 7,
+      suit: "Diamonds",
+      value: 7,
+      src: "./cards/7_of_diamonds.png",
+    },
+    {
+      type: 8,
+      suit: "Spades",
+      value: 8,
+      src: "./cards/8_of_spades.png",
+    },
+    {
+      type: 8,
+      suit: "Clubs",
+      value: 8,
+      src: "./cards/8_of_clubs.png",
+    },
+    {
+      type: 8,
+      suit: "Hearts",
+      value: 8,
+      src: "./cards/8_of_hearts.png",
+    },
+    {
+      type: 8,
+      suit: "Diamonds",
+      value: 8,
+      src: "./cards/8_of_diamonds.png",
+    },
+    {
+      type: 9,
+      suit: "Spades",
+      value: 9,
+      src: "./cards/9_of_spades.png",
+    },
+    {
+      type: 9,
+      suit: "Clubs",
+      value: 9,
+      src: "./cards/9_of_clubs.png",
+    },
+    {
+      type: 9,
+      suit: "Hearts",
+      value: 9,
+      src: "./cards/9_of_hearts.png",
+    },
+    {
+      type: 9,
+      suit: "Diamonds",
+      value: 9,
+      src: "./cards/9_of_diamonds.png",
+    },
+    {
+      type: 10,
+      suit: "Spades",
+      value: 10,
+      src: "./cards/10_of_spades.png",
+    },
+    {
+      type: 10,
+      suit: "Clubs",
+      value: 10,
+      src: "./cards/10_of_clubs.png",
+    },
+    {
+      type: 10,
+      suit: "Hearts",
+      value: 10,
+      src: "./cards/10_of_hearts.png",
+    },
+    {
+      type: 10,
+      suit: "Diamonds",
+      value: 10,
+      src: "./cards/10_of_diamonds.png",
+    },
+    {
+      type: "Jack",
+      suit: "Spades",
+      value: 10,
+      src: "./cards/jack_of_spades2.png",
+    },
+    {
+      type: "Jack",
+      suit: "Clubs",
+      value: 10,
+      src: "./cards/jack_of_clubs2.png",
+    },
+    {
+      type: "Jack",
+      suit: "Hearts",
+      value: 10,
+      src: "./cards/jack_of_hearts2.png",
+    },
+    {
+      type: "Jack",
+      suit: "Diamonds",
+      value: 10,
+      src: "./cards/jack_of_diamonds2.png",
+    },
+    {
+      type: "Queen",
+      suit: "Spades",
+      value: 10,
+      src: "./cards/queen_of_spades2.png",
+    },
+    {
+      type: "Queen",
+      suit: "Clubs",
+      value: 10,
+      src: "./cards/queen_of_clubs2.png",
+    },
+    {
+      type: "Queen",
+      suit: "Hearts",
+      value: 10,
+      src: "./cards/queen_of_hearts2.png",
+    },
+    {
+      type: "Queen",
+      suit: "Diamonds",
+      value: 10,
+      src: "./cards/queen_of_diamonds2.png",
+    },
+    {
+      type: "King",
+      suit: "Spades",
+      value: 10,
+      src: "./cards/king_of_spades2.png",
+    },
+    {
+      type: "King",
+      suit: "Clubs",
+      value: 10,
+      src: "./cards/king_of_clubs2.png",
+    },
+    {
+      type: "King",
+      suit: "Hearts",
+      value: 10,
+      src: "./cards/king_of_hearts2.png",
+    },
+    {
+      type: "King",
+      suit: "Diamonds",
+      value: 10,
+      src: "./cards/king_of_diamonds2.png",
+    },
+  ],
+];
+
+let list = [];
+for (let i = 1; i <= 13; i++) {
+  let value = i;
+  let type = i;
+  switch (type) {
+    case 1:
+      type = "Ace";
+      value = 1;
+      break;
+    case 11:
+      type = "Jack";
+      value = 10;
+      break;
+    case 12:
+      type = "Queen";
+      value = 10;
+      break;
+    case 13:
+      type = "King";
+      value = 10;
+  }
+  for (let x = 0; x < 4; x++) {
+    let suit = "";
+    switch (x) {
+      case 0:
+        suit = "Spades";
+        break;
+      case 1:
+        suit = "Clubs";
+        break;
+      case 2:
+        suit = "Hearts";
+        break;
+      case 3:
+        suit = "Diamonds";
+        break;
+    }
+    let src = ``;
+    if (typeof type === "string") {
+      src = `./cards/${type.toLowerCase()}_of_${suit.toLowerCase()}2.png`;
+    } else {
+      src = `./cards/${type}_of_${suit.toLowerCase()}.png`;
+    }
+    list.push({ type: type, suit: suit, value: value, src: src });
+  }
+}
--- blackJack/index.js
+++ blackJack/index.js
@@ -1,7 +1,144 @@
-import { response } from "express";
 import "./style.css";
+import cardList from "./cardList";
 
-async function getDeck(string) {
-  let data = await fetch(string).then((response) => response.json);
-  console.log(data);
+let cards = cardList[0],
+  cardAmmount = 0,
+  hand = [],
+  bust = false,
+  stand = false,
+  ace = false,
+  playerTotal = 0;
+
+let DOM = {
+  dealer1: document.querySelector("#dealer1"),
+  dealer2: document.querySelector("#dealer2"),
+  dealerValue: document.querySelector("#dealer-value"),
+  dealerCards: document.querySelector("#dealerCards"),
+  player1: document.querySelector("#player1"),
+  player2: document.querySelector("#player2"),
+  playerValue: document.querySelector("#player-value"),
+  playerCards: document.querySelector("#playerCards"),
+  hit: document.querySelector("#hit-button"),
+  stand: document.querySelector("#stand-button"),
+  bust: document.querySelector(".bust"),
+  newGame: document.querySelector("#newGame"),
+  img: document.querySelector("#bust"),
+};
+let ogDiv = DOM.dealerCards.innerHTML;
+
+window.onload = () => startGame();
+
+function startGame() {
+  cards = shuffle(cards);
+  DOM.dealerCards.innerHTML = "";
+  DOM.dealerCards.insertAdjacentHTML(
+    "afterbegin",
+    `<img src="cards/card_rear.png" alt="card1" id="dealer1">
+    <img src=${cards[1].src} alt="card2" id="dealer2">`
+  );
+  DOM.dealer1 = document.querySelector("#dealer1");
+  DOM.playerCards.innerHTML = `
+  <img src=${cards[2].src} alt="card1" id="player1">
+  <img src=${cards[3].src} alt="card2" id="player2">`;
+  hand = [];
+  bust = false;
+  stand = false;
+  ace = false;
+  playerTotal = 0;
+  DOM.bust.setAttribute("hidden", "");
+  DOM.dealer1.src = "./cards/card_rear.png";
+  DOM.dealer2.src = cards[1].src;
+  DOM.dealerValue.innerHTML = cards[1].value;
+  DOM.playerValue.innerHTML = cards[2].value + cards[3].value;
+  cardAmmount = 2;
+  hand.push(cards[2], cards[3]);
+}
+
+function checkGame() {
+  if (hand.length === 2 && hand[0].value + hand[1].value === 21) {
+    playerTotal = 21.5;
+    stand = true;
+    console.log("black jack");
+    return;
+  }
+  ace = hand.some((card) => card.type === "Ace");
+  playerTotal = 0;
+  for (let i = 0; i <= cardAmmount - 1; i++) {
+    playerTotal += hand[i].value;
+
+    if (playerTotal > 21) {
+      console.log(ace);
+      if (ace) {
+        playerTotal -= 10;
+        ace = false;
+      } else {
+        DOM.img.src = "./cards/bust.png";
+
+        DOM.bust.removeAttribute("hidden");
+        bust = true;
+        break;
+      }
+    }
+  }
+  DOM.playerValue.innerHTML = playerTotal;
+}
+
+DOM.stand.addEventListener("click", () => {
+  checkGame();
+  stand = true;
+  DOM.dealer1.src = cards[0].src;
+  let dealerTotal = 0;
+  if (cards[0].value + cards[1].value === 21) {
+    dealerTotal = 21;
+  } else {
+    dealerTotal = cards[0].value + cards[1].value;
+  }
+  let itterator = 51;
+  while (dealerTotal < 17) {
+    DOM.dealerCards.insertAdjacentHTML(
+      "beforeend",
+      `<img src="${cards[itterator].src}">`
+    );
+    dealerTotal += cards[itterator].value;
+    itterator--;
+  }
+  DOM.dealerValue.innerHTML = dealerTotal;
+  if (dealerTotal > 21) {
+    dealerTotal = 0;
+  }
+  DOM.dealer1.src = cards[0].src;
+  console.log(playerTotal, dealerTotal);
+  if (dealerTotal < playerTotal) {
+    DOM.img.src = "./cards/win.png";
+    DOM.bust.removeAttribute("hidden");
+  } else if (dealerTotal == playerTotal) {
+    DOM.img.src = "./cards/tie.png";
+    DOM.bust.removeAttribute("hidden");
+  } else {
+    DOM.img.src = "./cards/lose.webp";
+    DOM.bust.removeAttribute("hidden");
+  }
+});
+
+DOM.hit.addEventListener("click", () => {
+  if (bust || stand) {
+    return;
+  }
+  cardAmmount++;
+  DOM.playerCards.insertAdjacentHTML(
+    "beforeend",
+    `<img src="${cards[cardAmmount + 1].src}">`
+  );
+  hand.push(cards[cardAmmount + 1]);
+  checkGame();
+});
+
+DOM.newGame.addEventListener("click", () => startGame());
+
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
 }
